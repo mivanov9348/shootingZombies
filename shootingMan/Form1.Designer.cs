565643475e6ff1a3f7d99a306ca9abe0b8a4be10
mivanov9348 <mivanov93@abv.bg>
2021-01-03 16:52:08 +0200

    ShootingManProject commit

---
+++ shootingMan/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace shootingMan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.enemyThree = new System.Windows.Forms.PictureBox();
+            this.enemyTwo = new System.Windows.Forms.PictureBox();
+            this.bullet = new System.Windows.Forms.PictureBox();
+            this.Kain = new System.Windows.Forms.PictureBox();
+            this.enemyOne = new System.Windows.Forms.PictureBox();
+            this.score = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.enemyThree)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyTwo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Kain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyOne)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // enemyThree
+            // 
+            this.enemyThree.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.enemyThree.Cursor = System.Windows.Forms.Cursors.Default;
+            this.enemyThree.Image = global::shootingMan.Properties.Resources._8_2_zombie_png;
+            this.enemyThree.Location = new System.Drawing.Point(639, 2);
+            this.enemyThree.Name = "enemyThree";
+            this.enemyThree.Size = new System.Drawing.Size(111, 183);
+            this.enemyThree.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.enemyThree.TabIndex = 6;
+            this.enemyThree.TabStop = false;
+            // 
+            // enemyTwo
+            // 
+            this.enemyTwo.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.enemyTwo.Cursor = System.Windows.Forms.Cursors.Default;
+            this.enemyTwo.Image = global::shootingMan.Properties.Resources._8_2_zombie_png;
+            this.enemyTwo.Location = new System.Drawing.Point(373, 12);
+            this.enemyTwo.Name = "enemyTwo";
+            this.enemyTwo.Size = new System.Drawing.Size(111, 183);
+            this.enemyTwo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.enemyTwo.TabIndex = 5;
+            this.enemyTwo.TabStop = false;
+            // 
+            // bullet
+            // 
+            this.bullet.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.bullet.Image = global::shootingMan.Properties.Resources.bullets_PNG35565;
+            this.bullet.Location = new System.Drawing.Point(384, 393);
+            this.bullet.Name = "bullet";
+            this.bullet.Size = new System.Drawing.Size(37, 33);
+            this.bullet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bullet.TabIndex = 4;
+            this.bullet.TabStop = false;
+            // 
+            // Kain
+            // 
+            this.Kain.Image = global::shootingMan.Properties.Resources.shoot;
+            this.Kain.Location = new System.Drawing.Point(358, 443);
+            this.Kain.Name = "Kain";
+            this.Kain.Size = new System.Drawing.Size(82, 79);
+            this.Kain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Kain.TabIndex = 3;
+            this.Kain.TabStop = false;
+            // 
+            // enemyOne
+            // 
+            this.enemyOne.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.enemyOne.Cursor = System.Windows.Forms.Cursors.Default;
+            this.enemyOne.Image = global::shootingMan.Properties.Resources._8_2_zombie_png;
+            this.enemyOne.Location = new System.Drawing.Point(85, 12);
+            this.enemyOne.Name = "enemyOne";
+            this.enemyOne.Size = new System.Drawing.Size(111, 183);
+            this.enemyOne.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.enemyOne.TabIndex = 0;
+            this.enemyOne.TabStop = false;
+            // 
+            // score
+            // 
+            this.score.AutoSize = true;
+            this.score.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.score.ForeColor = System.Drawing.Color.Red;
+            this.score.Location = new System.Drawing.Point(12, 281);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(24, 25);
+            this.score.TabIndex = 7;
+            this.score.Text = "0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(821, 545);
+            this.Controls.Add(this.score);
+            this.Controls.Add(this.enemyThree);
+            this.Controls.Add(this.enemyTwo);
+            this.Controls.Add(this.Kain);
+            this.Controls.Add(this.enemyOne);
+            this.Controls.Add(this.bullet);
+            this.Name = "Form1";
+            this.Text = "Shhoooot";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.enemyThree)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyTwo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Kain)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyOne)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox enemyOne;
+        private System.Windows.Forms.PictureBox Kain;
+        private System.Windows.Forms.PictureBox bullet;
+        private System.Windows.Forms.PictureBox enemyTwo;
+        private System.Windows.Forms.PictureBox enemyThree;
+        private System.Windows.Forms.Label score;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ shootingMan/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace shootingMan
+{
+    public partial class Form1 : Form
+    {
+
+        bool goLeft, goRight, shooting, isGameOver;
+        int playerScore;
+        int playerSpeed = 10;
+        int enemySpeed;
+        int bulletSpeed;
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            resetGame();
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+
+            score.Text = playerScore.ToString();
+            enemyOne.Top += enemySpeed;
+            enemyTwo.Top += enemySpeed;
+            enemyThree.Top += enemySpeed;
+
+            if (enemyOne.Top > 250 || enemyTwo.Top > 250 || enemyThree.Top > 250)
+            {
+                score.BringToFront();
+                gameOver();
+                
+            }
+            if (goLeft == true && Kain.Left > 0)
+            {
+                Kain.Left -= playerSpeed;
+            }
+            if (goRight == true && Kain.Left < 550)
+            {
+                Kain.Left += playerSpeed;
+            }
+            if (shooting == true)
+            {
+                bulletSpeed = 20;
+                bullet.Top -= bulletSpeed;
+            }
+            else
+            {
+                bullet.Left = -300;
+                bulletSpeed = 0;
+            }
+            if (bullet.Top < 50)
+            {
+                shooting = false;
+            }
+            if (bullet.Bounds.IntersectsWith(enemyOne.Bounds))
+            {
+                playerScore += 1;
+                enemyOne.Top = -30;
+                enemyOne.Left = rnd.Next(-20, 200);
+                shooting = false;
+            }
+            if (bullet.Bounds.IntersectsWith(enemyTwo.Bounds))
+            {
+                playerScore += 1;
+                enemyTwo.Top = -30;
+                enemyTwo.Left = rnd.Next(-20, 500);
+                shooting = false;
+            }
+            if (bullet.Bounds.IntersectsWith(enemyThree.Bounds))
+            {
+                playerScore += 1;
+                enemyThree.Top = -30;
+                enemyThree.Left = rnd.Next(-20, 200);
+                shooting = false;
+            }
+            if (playerScore == 10)
+            {
+                enemySpeed = 2;
+                playerSpeed = 15;
+                bulletSpeed = 30;
+            }
+            if (playerScore == 50)
+            {
+                enemySpeed = 3;
+                playerSpeed = 20;
+                bulletSpeed = 40;
+            }
+            if (playerScore == 100)
+            {
+                enemySpeed = 4;
+                playerSpeed = 30;
+                bulletSpeed = 50;
+            }
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (e.KeyCode == Keys.Space && shooting == false)
+            {
+                shooting = true;
+                bullet.Top = Kain.Top - bullet.Height;
+                bullet.Left = Kain.Left + 20;
+            }
+            if (e.KeyCode == Keys.Enter && isGameOver == true)
+            {
+                resetGame();
+            }
+        }
+        private void resetGame()
+        {
+            gameTimer.Start();
+            enemySpeed = 1;
+
+            enemyOne.Left = rnd.Next(20, 500);
+            enemyTwo.Left = rnd.Next(20, 500);
+            enemyThree.Left = rnd.Next(20, 500);
+
+            enemyOne.Top = rnd.Next(250, 300) * -1;
+            enemyTwo.Top = rnd.Next(250, 300) * -1;
+            enemyThree.Top = rnd.Next(250, 300) * -1;
+
+            playerScore = 0;
+            bulletSpeed = 2;
+            bullet.Left = -300;
+            shooting = false;
+            score.Text = playerScore.ToString();
+        }
+        private void gameOver()
+        {
+            
+            isGameOver = true;
+            gameTimer.Stop();            
+            score.Text += Environment.NewLine + "Game Over" + Environment.NewLine + $"Result: {score.Text}" + Environment.NewLine + "Press Enter to try again.";
+            
+
+
+        }
+    }
+}
